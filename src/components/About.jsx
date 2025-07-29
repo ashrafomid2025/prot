34a5a6182653204775c467689484ed80fa160b8d
ashrafomid2025 <omidmashraf@gmail.com>
2025-07-29 08:23:52 +0430

    foreground

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ export default function About() {
               mentoring aspiring developers.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center ">
-              <a href="#contact" className="cosmic-button">
+              <a href="#contacts" className="cosmic-button">
                 Get in touch
               </a>
               <a
