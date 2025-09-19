24a3e62985e918d75374c264e08f88655976002f
ashrafomid2025 <omidmashraf@gmail.com>
2025-09-19 18:52:34 +0430

    changes in card styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -60,7 +60,7 @@ export default function Projects() {
               key={key}
               className="group bg-card rounded-lg p-4 overflow-hidden shadow-xs card-hover"
             >
-              <div className="h-40 shadow-2xs shadow-primary overflow-hidden ">
+              <div className="h-40 shadow-2xs mt-4 shadow-primary overflow-hidden ">
                 <img
                   src={project.imgUrl}
                   alt={project.title}
