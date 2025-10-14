e05e03193edff424961c5db42e8ddded230c5274
ashrafomid2025 <omidmashraf@gmail.com>
2025-10-14 06:13:31 +0430

    changes in heading text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,10 @@ export default function About() {
     <section id="about" className="py-24 px-4 relative">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
-          About <span className="text-primary">Me</span>
+          About{" "}
+          <span className="bg-gradient-to-l  from-primary/80 to-primary via-stone-200 bg-clip-text text-transparent">
+            Me
+          </span>
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6  ">
